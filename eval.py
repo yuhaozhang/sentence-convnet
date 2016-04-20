Fixed the double-softmax bug; cleaned up the code a bit.

eval.py
@@ -10,43 +10,47 @@ import text_input
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('data_dir', './data/mr/', 'Directory of the data')
-tf.app.flags.DEFINE_string('checkpoint_dir', './train/', 'Where to read model')
+tf.app.flags.DEFINE_string('train_dir', './train/', 'Where to read model')
 
 def evaluate():
     """ Build evaluation graph and run. """
     with tf.Graph().as_default():
-        sentences = tf.placeholder(dtype=tf.int64, shape=[FLAGS.batch_size, model.SENT_LENGTH], name='input_x')
+        sentences = tf.placeholder(dtype=tf.int64, shape=[FLAGS.batch_size, FLAGS.sent_len], name='input_x')
         labels = tf.placeholder(dtype=tf.int64, shape=[FLAGS.batch_size], name='input_y')
         keep_prob = tf.placeholder(dtype=tf.float32, shape=[], name='keep_prob')
 
         logits = model.inference(sentences, keep_prob)
         correct_prediction = tf.to_int32(tf.nn.in_top_k(logits, labels, 1))
         true_count_op = tf.reduce_sum(correct_prediction)
+        loss = model.loss(logits, labels)
 
         saver = tf.train.Saver(tf.all_variables())
 
         # read test files
-        x, y = text_input.load_data_from_dump(FLAGS.data_dir, 'test.cPickle')
+        x, y = text_input.load_from_dump(FLAGS.data_dir, 'test.cPickle')
         loader = text_input.DataLoader(x, y, batch_size=FLAGS.batch_size)
         num_batches = loader.batches_per_epoch()
         print 'Start evaluation, %d batches needed, with %d examples per batch.' % (num_batches, FLAGS.batch_size)
 
-        true_count_total = 0
+        true_count = 0
+        test_loss = 0
 
         with tf.Session() as sess:
-            ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+            ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
             if ckpt and ckpt.model_checkpoint_path:
                 saver.restore(sess, ckpt.model_checkpoint_path)
             else:
-                print "No checkpoint file found!"
-                return
+                raise IOError("Loading checkpoint file failed!")
 
             for step in range(num_batches):
                 x, y = loader.next_batch()
-                true_count_total += sess.run(true_count_op, feed_dict={keep_prob:1.0, sentences:x, labels:y})
+                true_count_value, loss_value = sess.run([true_count_op, loss], feed_dict={keep_prob:1.0, sentences:x, labels:y})
+                true_count += true_count_value
+                test_loss += loss_value
 
-            precision = float(true_count_total) / loader.num_examples
-            print '%s: precision = %.3f' % (datetime.now(), precision)
+            precision = float(true_count) / loader.num_examples
+            test_loss = float(test_loss) / num_batches
+            print '%s: test_loss = %.6f, test_precision = %.3f' % (datetime.now(), test_loss, precision)
 
 def main(argv=None):
     evaluate()

model.py
@@ -8,9 +8,8 @@ tf.app.flags.DEFINE_integer('num_kernel', 100, 'Number of filters for each windo
 tf.app.flags.DEFINE_integer('min_window', 3, 'Minimum size of filter window')
 tf.app.flags.DEFINE_integer('max_window', 5, 'Maximum size of filter window')
 tf.app.flags.DEFINE_integer('vocab_size', 18000, 'Vocabulary size')
-
-NUM_CLASSES = 2
-SENT_LENGTH = 59
+tf.app.flags.DEFINE_integer('num_class', 2, 'Number of class to consider')
+tf.app.flags.DEFINE_integer('sent_len', 59, 'Input sentence length. This is after the padding is performed.')
 
 def _variable_on_cpu(name, shape, initializer):
     with tf.device('/cpu:0'):
@@ -41,7 +40,7 @@ def inference(sentences, keep_prob):
             kernel = _variable_on_cpu(name='kernel_'+str(k_size),
                 shape=[k_size, FLAGS.emb_size, 1, FLAGS.num_kernel], initializer=tf.truncated_normal_initializer(stddev=0.01))
             conv = tf.nn.conv2d(input=sent_batch, filter=kernel, strides=[1,1,1,1], padding='VALID')
-            biases = _variable_on_cpu('biases'+str(k_size), [FLAGS.num_kernel], tf.constant_initializer(0.0))
+            biases = _variable_on_cpu('biases_'+str(k_size), [FLAGS.num_kernel], tf.constant_initializer(0.0))
             bias = tf.nn.bias_add(conv, biases)
             relu = tf.nn.relu(bias, name=scope.name)
             # shape of relu: [batch_size, conv_len, 1, num_kernel]
@@ -59,11 +58,11 @@ def inference(sentences, keep_prob):
     # fully-connected layer
     pool_size = (FLAGS.max_window - FLAGS.min_window + 1) * FLAGS.num_kernel
     with tf.variable_scope('fc') as scope:
-        W = _variable_on_cpu('W', shape=[pool_size, NUM_CLASSES],
-            initializer=tf.truncated_normal_initializer(stddev=0.01))
-        biases = _variable_on_cpu('biases', [NUM_CLASSES], tf.constant_initializer(0.0))
-        logits = tf.nn.softmax(tf.add(tf.matmul(pool_dropout, W), biases), name=scope.name)
-        _activation_summary(logits, name='softmax')
+        W = _variable_on_cpu('W', shape=[pool_size, FLAGS.num_class],
+            initializer=tf.truncated_normal_initializer(stddev=0.05))
+        biases = _variable_on_cpu('biases', [FLAGS.num_class], tf.constant_initializer(0.01))
+        logits = tf.nn.bias_add(tf.matmul(pool_dropout, W), biases)
+        _activation_summary(logits, name='logits')
 
     return logits
 
@@ -74,7 +73,7 @@ def loss(logits, labels):
     return cross_entropy_loss
 
 def train_batch(loss, global_step, lr):
-    opt = tf.train.GradientDescentOptimizer(lr)
+    opt = tf.train.AdamOptimizer(lr)
     grads = opt.compute_gradients(loss)
     train_op = opt.apply_gradients(grads, global_step=global_step)
 
@@ -85,7 +84,7 @@ def train_batch(loss, global_step, lr):
         tf.histogram_summary(var.op.name, var)
 
     for grad, var in grads:
-        if grad:
+        if grad is not None:
             tf.histogram_summary(var.op.name + '/gradients', grad)
 
     return train_op

text_input.py
@@ -7,6 +7,7 @@ import numpy as np
 
 UNK_TOKEN = '<unk>'
 PAD_TOKEN = '<pad>'
+RANDOM_SEED = 123
 
 class TextReader(object):
 
@@ -79,13 +80,14 @@ class TextReader(object):
         return sentences_and_labels
 
     def shuffle_and_split(self, sentences_and_labels, test_fraction=0.2):
+        random.seed(RANDOM_SEED)
         random.shuffle(sentences_and_labels)
         self.num_examples = len(sentences_and_labels)
         test_num = int(self.num_examples * test_fraction)
         self.test_data = sentences_and_labels[:test_num]
         self.train_data = sentences_and_labels[test_num:]
-        dump_data(self.data_dir, 'train.cPickle', self.train_data)
-        dump_data(self.data_dir, 'test.cPickle', self.test_data)
+        dump_to_file(self.data_dir, 'train.cPickle', self.train_data)
+        dump_to_file(self.data_dir, 'test.cPickle', self.test_data)
         print 'Split dataset into training and test set: %d for training, %d for testing.' % \
             (self.num_examples - test_num, test_num)
         return
@@ -126,13 +128,13 @@ class DataLoader(object):
         return int(np.ceil(self.num_examples / self.batch_size))
 
 
-def dump_data(data_dir, filename, data):
+def dump_to_file(data_dir, filename, obj):
     dump_file = os.path.join(data_dir, filename)
     with open(dump_file, 'w') as outfile:
-        pickle.dump(data, file=outfile)
+        pickle.dump(obj, file=outfile)
     return
 
-def load_data_from_dump(data_dir, filename):
+def load_from_dump(data_dir, filename):
     dump_file = os.path.join(data_dir, filename)
     with open(dump_file, 'r') as infile:
         data = pickle.load(infile)

train.py
@@ -13,21 +13,20 @@ tf.app.flags.DEFINE_string('data_dir', './data/mr/', 'Directory of the data')
 tf.app.flags.DEFINE_string('train_dir', './train/', 'Directory to save training checkpoint files')
 tf.app.flags.DEFINE_integer('max_steps', 10000, 'Max number of steps to run')
 tf.app.flags.DEFINE_boolean('log_device_placement', False, 'Whether log device information in summary')
-
-LOG_STEP_INTERVAL = 10
-SUMMARY_STEP_INTERVAL = 100
-SAVE_STEP_INTERVAL = 1000
-
-INITIAL_LR = 0.4
-LR_DECAY_RATE = 0.9
-MAX_STEP_LOSS_UNCHANGED = 200
-TRAIN_DROPOUT_RATE = 0.5
+tf.app.flags.DEFINE_float('init_lr', 0.01, 'Initial learning rate')
+tf.app.flags.DEFINE_float('lr_decay', 0.9, 'LR decay rate')
+tf.app.flags.DEFINE_integer('tolerance_step', 200, 'Decay the lr after loss remains unchanged for this number of steps')
+tf.app.flags.DEFINE_float('dropout', 0.5, 'Dropout rate. 0 is no dropout.')
+tf.app.flags.DEFINE_integer('log_step', 10, 'Write log to stdout after this step')
+tf.app.flags.DEFINE_integer('summary_step', 100, 'Write summary after this step')
+tf.app.flags.DEFINE_integer('save_step', 1000, 'Save model after this step')
+tf.app.flags.DEFINE_float('test_fraction', 0.15, 'Test set fraction')
 
 def train():
     with tf.Graph().as_default():
         global_step = tf.Variable(0, trainable=False)
 
-        sentences = tf.placeholder(dtype=tf.int64, shape=[FLAGS.batch_size, model.SENT_LENGTH], name='input_x')
+        sentences = tf.placeholder(dtype=tf.int64, shape=[FLAGS.batch_size, FLAGS.sent_len], name='input_x')
         labels = tf.placeholder(dtype=tf.int64, shape=[FLAGS.batch_size], name='input_y')
         lr = tf.placeholder(dtype=tf.float32, shape=[], name='learning_rate')
         keep_prob = tf.placeholder(dtype=tf.float32, shape=[], name='keep_prob')
@@ -44,40 +43,40 @@ def train():
         sess.run(tf.initialize_all_variables())
         summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, graph_def=sess.graph_def)
 
-        current_lr = INITIAL_LR
+        current_lr = FLAGS.init_lr
         lowest_loss_value = float("inf")
         step_loss_ascend = 0
+        save_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
 
         # loading data
         reader = text_input.TextReader(FLAGS.data_dir, suffix_list=['neg', 'pos'])
-        reader.prepare_data(vocab_size=FLAGS.vocab_size)
+        reader.prepare_data(vocab_size=FLAGS.vocab_size, test_fraction=FLAGS.test_fraction)
         x, y = reader.get_data_and_labels()
         loader = text_input.DataLoader(x, y, batch_size=FLAGS.batch_size)
 
         for step in xrange(FLAGS.max_steps):
             start_time = time.time()
             x_batch, y_batch = loader.next_batch()
-            dict_to_feed = {sentences: x_batch, labels: y_batch, lr: current_lr, keep_prob: TRAIN_DROPOUT_RATE}
+            dict_to_feed = {sentences: x_batch, labels: y_batch, lr: current_lr, keep_prob: (1.-FLAGS.dropout)}
             _, loss_value = sess.run([train_op, loss], feed_dict=dict_to_feed)
             duration = time.time() - start_time
 
             assert not np.isnan(loss_value), "Model loss is NaN."
 
-            if step % LOG_STEP_INTERVAL == 0:
+            if step % FLAGS.log_step == 0:
                 examples_per_sec = FLAGS.batch_size / duration
                 sec_per_batch = float(duration)
 
-                format_str = ('%s: step %d/%d, loss = %.2f (%.1f examples/sec; %.3f sec/batch), lr: %.5f')
+                format_str = ('%s: step %d/%d, loss = %.6f (%.1f examples/sec; %.3f sec/batch), lr: %.6f')
                 print (format_str % (datetime.now(), step, FLAGS.max_steps, loss_value, examples_per_sec, 
                     sec_per_batch, current_lr))
 
-            if step % SUMMARY_STEP_INTERVAL == 0:
+            if step % FLAGS.summary_step == 0:
                 summary_str = sess.run(summary_op, feed_dict=dict_to_feed)
                 summary_writer.add_summary(summary_str, step)
 
-            if step % SAVE_STEP_INTERVAL == 0:
-                ckpt_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
-                saver.save(sess, ckpt_path, global_step=step)
+            if step % FLAGS.save_step == 0:
+                saver.save(sess, save_path, global_step=step)
 
             # decay learning rate if necessary
             if loss_value < lowest_loss_value:
@@ -85,10 +84,14 @@ def train():
                 step_loss_ascend = 0
             else:
                 step_loss_ascend += 1
-            if step_loss_ascend >= MAX_STEP_LOSS_UNCHANGED:
-                current_lr *= LR_DECAY_RATE
+            if step_loss_ascend >= FLAGS.tolerance_step:
+                current_lr *= FLAGS.lr_decay
                 print '%s: step %d/%d, LR decays to %.5f' % ((datetime.now(), step, FLAGS.max_steps, current_lr))
                 step_loss_ascend = 0
+
+            # stop learning if learning rate is too low
+            if current_lr < 1e-5: break
+        saver.save(sess, save_path, global_step=step)
 
 def main(argv=None):
     if tf.gfile.Exists(FLAGS.train_dir):